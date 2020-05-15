1cacd1ba05dc787444f714e389e458f175e9adc7
Sam Gleske <sam.mxracer@gmail.com>
2020-05-14 20:08:57 -0400

    Add a couple tests

--- src/test/groovy/cloneable/AppLogicTest.groovy
+++ src/test/groovy/cloneable/AppLogicTest.groovy
@@ -18,5 +18,35 @@ class AppLogicTest extends Specification {
         then:
         result == '"foo"'
     }
+    def "Check if has next page"() {
+        setup:
+        Map response = [:]
+        response.data = [:]
+        response.data.owned = [:]
+        response.data.owned.repositories = [:]
+        response.data.owned.repositories.pages = [:]
+        response.data.owned.repositories.pages.hasNextPage = true
+
+        when:
+        Boolean result = AppLogic.hasNextPage(response)
+
+        then:
+        result == true
+    }
+    def "Check if no next page"() {
+        setup:
+        Map response = [:]
+        response.data = [:]
+        response.data.owned = [:]
+        response.data.owned.repositories = [:]
+        response.data.owned.repositories.pages = [:]
+        response.data.owned.repositories.pages.hasNextPage = false
+
+        when:
+        Boolean result = AppLogic.hasNextPage(response)
+
+        then:
+        result == false
+    }
 }
 
