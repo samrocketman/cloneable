8f4086d20691075e2d33375d3806d102b3bcb775
Sam Gleske <sam.mxracer@gmail.com>
2025-03-26 20:41:25 -0400

    Bump 2025 license

--- src/main/groovy/cloneable/App.groovy
+++ src/main/groovy/cloneable/App.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 Sam Gleske
+ * Copyright 2020-2025 Sam Gleske
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
